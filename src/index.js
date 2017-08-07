57339cec3f8fe7bce57d0fcc2ac44124b81f2dd8
Lou Gaeta <louis.gaeta1@gmail.com>
2017-08-07 12:32:36 -0400

    Create class-based search bar

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+
+const API_KEY = "AIzaSyARYQv6aqrPCpkukslpcCXMVtbw6gWmkVc";
 
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );  
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
